5a6180c51456b480010416526722d8df509a33f7
Molly McCarron <mccarronmollye@gmail.com>
2019-06-07 02:49:53 -0400

    Added axios, hitting fake posts api

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 class ResourceList extends React.Component{
+  state = { resources: []}
+
+  async componentDidMount(){ //use async if using await
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`) //await says hold off until we complete making that request
+
+    this.setState({ resources: response.data })
+  }
+
   render(){
-    return <div>{this.props.resource}</div>
+    return <div>{this.state.resources.length}</div>
   }
 }
 
